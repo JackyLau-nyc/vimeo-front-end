c5a0584f2e7a47a6d9370a9efc79f4f55cc7bd70
Jacky Lau <jackylau.email@gmail.com>
2019-10-28 04:04:10 -0400

    touch-up

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -21,7 +21,7 @@ export default class Wheel extends Component {
 					        	<div>
 					            	<h5>Spiderman: Into the Spider-verse</h5>
 					            	<p>Teen Miles Morales becomes Spider-Man of his reality, crossing his path with five counterparts from other dimensions to stop a threat for all realities.</p>
-					            	<Button target="_blank" href="https://www.amazon.com/Spider-Man-Into-Spider-Verse-Liev-Schreiber/dp/B07L9YXWSW/ref=sr_1_1?keywords=into+the+spiderverse&qid=1572232151&sr=8-1" variant="warning" className="buttonProp">Buy Now</Button>
+					            	<Button target="_blank" href="https://www.amazon.com/Spider-Man-Into-Spider-Verse-Liev-Schreiber/dp/B07L9YXWSW/ref=sr_1_1?keywords=into+the+spiderverse&qid=1572232151&sr=8-1" variant="warning" className="buttonProp"> &#11041; Buy Now</Button>
 					            	<Button target="_blank" href="https://vimeo.com/356724117" variant="outline-warning" className="buttonProp">Watch Trailer</Button>
 					            </div>
 				        	</Col>
@@ -41,7 +41,7 @@ export default class Wheel extends Component {
 					        	<div>
 					            	<h5>Interstellar</h5>
 					            	<p>A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.</p>
-					            	<Button target="_blank" href="https://www.amazon.com/Interstellar-Matthew-McConaughey/dp/B00TU9UFTS/ref=sr_1_1?keywords=interstellar&qid=1572245618&sr=8-1" variant="light" className="buttonProp">Buy Now</Button>
+					            	<Button target="_blank" href="https://www.amazon.com/Interstellar-Matthew-McConaughey/dp/B00TU9UFTS/ref=sr_1_1?keywords=interstellar&qid=1572245618&sr=8-1" variant="light" className="buttonProp"> &#11041; Buy Now</Button>
 					            	<Button target="_blank" href="https://vimeo.com/108990817" variant="outline-light" className="buttonProp">Watch Trailer</Button>
 					            </div>
 				            </Col>
@@ -61,7 +61,7 @@ export default class Wheel extends Component {
 					        	<div>
 					            	<h5>Your Name</h5>
 					            	<p>Two strangers find themselves linked in a bizarre way. When a connection forms, will distance be the only thing to keep them apart?</p>
-					            	<Button target="_blank" href="https://www.amazon.com/Your-Name-Makoto-Shinkai/dp/B078SNJK8G/ref=sr_1_1?keywords=your+name&qid=1572245667&sr=8-1" variant="primary" className="buttonProp">Buy Now</Button>
+					            	<Button target="_blank" href="https://www.amazon.com/Your-Name-Makoto-Shinkai/dp/B078SNJK8G/ref=sr_1_1?keywords=your+name&qid=1572245667&sr=8-1" variant="primary" className="buttonProp"> &#11041; Buy Now</Button>
 					            	<Button target="_blank" href="https://vimeo.com/328762415" variant="outline-primary" className="buttonProp">Watch Trailer</Button>
 					        	</div>
 				        	</Col>
@@ -81,7 +81,7 @@ export default class Wheel extends Component {
 					        	<div>
 					            	<h5>Spirited Away</h5>
 					            	<p>During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.</p>
-					            	<Button target="_blank" href="https://www.amazon.com/Spirited-Away-Bluray-Combo-Blu-ray/dp/B074121MFR/ref=sr_1_2?keywords=spirited+away&qid=1572246436&sr=8-2" variant="dark" className="buttonProp">Buy Now</Button>
+					            	<Button target="_blank" href="https://www.amazon.com/Spirited-Away-Bluray-Combo-Blu-ray/dp/B074121MFR/ref=sr_1_2?keywords=spirited+away&qid=1572246436&sr=8-2" variant="dark" className="buttonProp"> &#11041; Buy Now</Button>
 					            	<Button target="_blank" href="https://vimeo.com/305752404" variant="outline-dark" className="buttonProp">Watch Trailer</Button>
 					        	</div>
 				        	</Col>
@@ -101,7 +101,7 @@ export default class Wheel extends Component {
 					        	<div>
 					            	<h5>Arrival</h5>
 					            	<p>A linguist works with the military to communicate with alien lifeforms after twelve mysterious spacecraft appear around the world.</p>
-					            	<Button target="_blank" href="https://www.amazon.com/Arrival-Amy-Adams/dp/B01M2C4NP8/ref=sr_1_1?keywords=arrival&qid=1572246369&sr=8-1" variant="secondary" className="buttonProp">Buy Now</Button>
+					            	<Button target="_blank" href="https://www.amazon.com/Arrival-Amy-Adams/dp/B01M2C4NP8/ref=sr_1_1?keywords=arrival&qid=1572246369&sr=8-1" variant="secondary" className="buttonProp"> &#11041; Buy Now</Button>
 					            	<Button target="_blank" href="https://vimeo.com/272254092" variant="outline-secondary" className="buttonProp">Watch Trailer</Button>
 					        	</div>
 				        	</Col>
